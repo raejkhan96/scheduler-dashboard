ea7c061901b9724c47cd2091c5319450358a1271
Rae J. Khan <raekhan18@gmail.com>
2021-11-12 18:26:19 -0500

    Add lifecycle to classes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,27 +2,35 @@ import React, { Component } from "react";
 import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
+import axios from "axios";
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+ } from "helpers/selectors"
+import { setInterview } from "helpers/reducers";
 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
@@ -30,15 +38,17 @@ const data = [
 
 class Dashboard extends Component {
   
-  constructor(props) {
-    super(props);
-
-    this.selectPanel = this.selectPanel.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.selectPanel = this.selectPanel.bind(this);
+  // }
 
   state = {
     loading: false,
-    focused: null
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {}
   };
 
   selectPanel(id) {
@@ -47,9 +57,51 @@ class Dashboard extends Component {
     }));
   }
 
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
+
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+  
+    this.socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+
+      if(typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState(previousState => 
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  componentWillUnmount() {
+    this.socket.close();
+  }
 
   render() {
-   
+   // pass in dash board, and if focused, find focused call
     const dashboardClasses = classnames("dashboard", {
       "dashboard--focused": this.state.focused
     });
@@ -60,6 +112,7 @@ class Dashboard extends Component {
 
     const panels = data
     .filter(
+      // pass in null or panel id
       panel => this.state.focused === null || this.state.focused === panel.id  
     )
     .map(panel => (
@@ -68,7 +121,8 @@ class Dashboard extends Component {
         id={panel.id}
         label={panel.label}
         value={panel.value}
-        onSelect={event => this.selectPanel(panel.id)}
+        // onSelect={event => this.selectPanel(panel.id)}
+        onSelect={() => this.selectPanel(panel.id)}
       />
     ));
 
